7effc9b169c545da1f942ef368460c1b2bd00e29
uklonsky <148268884+uklonsky@users.noreply.github.com>
2023-10-17 21:36:06 +0300

    uploud

---
+++ data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2023-10-17-at-201743",
+      "name": "WhatsApp Image 2023-10-17 at 20.17.43",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08842938443101289,
+          "pitch": -0.06262723551058791,
+          "rotation": 0,
+          "target": "0-whatsapp-image-2023-10-17-at-201743"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
